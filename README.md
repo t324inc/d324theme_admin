bf413044039f4747e56a047a484f9197f68d106f
Brian Nowell <briannowell@gmail.com>
2019-05-18 16:53:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# D324theme Admin
+---
+
+An admin theme based on Adminimal Theme for [D324](https://github.com/t324inc/d324).
+ [Varbase](https://www.drupal.org/project/varbase) standard websites.
+
+### Use With D324 Distribution:
+This theme is best used with D324 distribution.
+
+This theme is developed by [T324](https://www.t324.com).
---
+++ js/d324theme-admin.script.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Behaviors for the d324theme admin theme.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.d324themeAdmin = {
+    attach: function (context) {
+      // D324theme admin JavaScript behaviors goes here.
+    }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
